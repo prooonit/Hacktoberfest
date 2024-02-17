42201fdae555c26e4816d2092baa7ced2fb91a82
[pronit r] <[rahangdalejaykumar@gmail.com]>
2024-02-17 15:18:35 +0530

    check string length

---
+++ Languages/Java/CheckStringLength.java
@@ -0,0 +1,9 @@
+public class CheckStringLength {
+    public static void main(String[] args) {
+        //if string length is greater than 10  then print yes else print no
+
+        String s="Kunal Khsuhwah";
+        if(s.length()>10) System.out.println("yes");
+        else System.out.println("No");
+    }
+}
